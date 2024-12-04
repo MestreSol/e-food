7bca75d56423f7466bac5e25135d3b43c947d196
MestreSol <mb.muitobosta@gmail.com>
2024-12-04 18:37:39 -0300

    🚧 feat: implement routing with react-router-dom and add Home and About pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import logo from './logo.svg'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
   return (
-    <></>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   )
 }
 

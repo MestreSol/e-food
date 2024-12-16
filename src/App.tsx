77edfc66ba5bc3aa9b2a2ae85b13da1291045b2c
MestreSol <mb.muitobosta@gmail.com>
2024-12-16 13:12:37 -0300

    feat: implement routing with react-router-dom for Products and FilteredProducts components

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,21 @@ import SearchArea from './containers/searchArea'
 import ProductsAreaComponent from './containers/ProductsArea'
 import Footer from './containers/Footer/indext'
 import GlobalStyle from './GlobalStyle'
+import mockProducts from './mocks/mockProducts'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import FilteredProducts from './containers/FilteredProducts'
 
 function App() {
   return (
-    <>
+  <Router>
       <GlobalStyle></GlobalStyle>
       <Header></Header>
-      <Carousel></Carousel>
-      <SearchArea></SearchArea>
-      <ProductsAreaComponent></ProductsAreaComponent>
+      <Routes>
+        <Route path="/" element={<ProductsAreaComponent Products={mockProducts} />}></Route>
+        <Route path='/filtered-products' Component={FilteredProducts}></Route>
+      </Routes>
       <Footer></Footer>
-    </>
+  </Router>
   )
 }
 
